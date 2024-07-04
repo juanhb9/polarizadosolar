ee6d397001419403b836996e9ff2cefcc189c4e5
juanhb9 <juanhb9@gmail.com>
2024-07-03 18:36:31 -0600

    mensaje whatsapp

--- js/whatsApp.js
+++ js/whatsApp.js
@@ -1,3 +1,4 @@
 document.getElementById("whatsapp-btn").addEventListener("click", function(){
-    window.open("https://wa.me/+525583253406?text=Enviame%20%20un%20Mensaje", "_blank");
+    /* window.open("https://wa.me/+525583253406?text=Enviame%20%20un%20Mensaje", "_blank"); */
+    window.open("https://wa.me/+525583253406?text=¡Hola! Estoy interesado en obtener más información sobre sus servicios de instalación de películas de seguridad y control solar. ¿Podrían ayudarme?", "_blank");
 });
